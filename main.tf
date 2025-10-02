00215387c93ca199f7efbbb75d3bbb83322796c6
Jeremy Michael Cerda <3@wilkesliberty.com>
2025-10-01 21:52:27 -0400

    chore: Apply consistent Terraform code formatting

--- main.tf
+++ main.tf
@@ -1,25 +1,25 @@
 terraform {
   required_version = ">= 1.0"
-  
+
   required_providers {
     njalla = {
       source  = "Sighery/njalla"
       version = "0.10.0"
     }
-    
+
     # Additional providers as needed for your infrastructure
     # Example providers that might be relevant:
-    
+
     # aws = {
     #   source  = "hashicorp/aws"
     #   version = "~> 5.0"
     # }
-    
+
     # digitalocean = {
     #   source  = "digitalocean/digitalocean"
     #   version = "~> 2.0"
     # }
-    
+
     # cloudflare = {
     #   source  = "cloudflare/cloudflare"
     #   version = "~> 4.0"
